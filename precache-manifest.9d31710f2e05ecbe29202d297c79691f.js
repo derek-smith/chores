b3aa6e03a00c19422af4ee676cec6b6121b4b217
Derek Smith <codewiththeflow@gmail.com>
2019-07-16 21:34:50 -0700

    Updates

---
+++ precache-manifest.9d31710f2e05ecbe29202d297c79691f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc27ee42518bffc4e79d0a51bae32a98",
+    "url": "/chores/index.html"
+  },
+  {
+    "revision": "67300b5f6bf89347f63d",
+    "url": "/chores/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/chores/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "67300b5f6bf89347f63d",
+    "url": "/chores/static/js/main.62fa8b8f.chunk.js"
+  },
+  {
+    "revision": "1cbad9cea7ddfbcaaf11",
+    "url": "/chores/static/js/runtime~main.047c0c15.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chores/static/media/logo.5d5d9eef.svg"
+  }
+]);
