d96a30d4583b2074c8c13d662e6d8179d4cc5ae5
Derek Smith <codewiththeflow@gmail.com>
2019-07-19 22:30:46 -0700

    master: Working on the styling...

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,9 @@ import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
-// import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
-import IconButton from '@material-ui/core/IconButton';
 import AddBoxIcon from '@material-ui/icons/AddBox';
+import PersonIcon from '@material-ui/icons/Person';
 
 const useStyles = makeStyles(() => ({
   centered: {
@@ -24,19 +23,39 @@ const useStyles = makeStyles(() => ({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  list: {
+    margin: '56px 0',
+    padding: '4px 0 0 0',
+  },
+  chore: {
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+  },
+  choreName: {
+    maxWidth: '50%',
+  },
+  count: {
+    flex: 'unset',
+  },
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  invisible: {
+    // visibility: 'hidden',
+    minWidth: 'unset',
+  },
   footer: {
     top: 'auto',
     bottom: 0,
   },
-  flip: {
-    display: 'flex',
-    flexDirection: 'row-reverse',
+  userName: {
+    marginLeft: 8,
   },
-  count: {
-    flex: 'unset',
-    marginLeft: 16,
-    marginRight: 32,
-  }
+  userNameButton: {
+    paddingLeft: 12,
+  },
 }));
 
 const App = ({choreList, getChoreList, isSignedIn, name, signIn}) => {
@@ -51,34 +70,32 @@ const App = ({choreList, getChoreList, isSignedIn, name, signIn}) => {
   return (
     <>
      <AppBar position="fixed" color="default">
-        <Toolbar>
+        <Toolbar classes={{root: classes.header}}>
           <Typography variant="h6" color="inherit">
             Chores
           </Typography>
+          {name && (
+            <Button variant="contained" color="primary" classes={{root: classes.userNameButton}}>
+              <PersonIcon />
+              <span className={classes.userName}>{name}</span>
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
       {(isSignedIn ? (
         !choreList.length
           ? `Hi ${name}`
           : (
-            <List>
+            <List dense={true} classes={{root: classes.list}}>
               {choreList.map(chore => (
-                <ListItem key={chore.name} onClick={() => {/* TODO */}}>
-                  <ListItemText disableTypography={true} classes={{root: classes.count}}>
-                    <Typography variant="h4" color="inherit">
-                      {chore.count}
-                    </Typography>
+                <ListItem key={chore.name} classes={{root: classes.chore}} onClick={() => console.log('ListItem onClick()')} button={true} divider={true}>
+                  <ListItemText classes={{root: classes.choreName}}>
+                    {chore.name}
                   </ListItemText>
-                  <ListItemText>
-                    {/* <Typography variant="h6" color="inherit"> */}
-                      {chore.name}
-                    {/* </Typography> */}
-                  </ListItemText>
-                  <ListItemIcon>
-                    <IconButton edge="end">
-                      <AddBoxIcon />
-                    </IconButton>
-                    </ListItemIcon>
+                  <ListItemText classes={{root: classes.count}}>{chore.count} @ ${(chore.price / 100)}</ListItemText>
+                  {/* <ListItemIcon classes={{root: classes.invisible}}>
+                    <IconButton edge="end"><AddBoxIcon /></IconButton>
+                  </ListItemIcon> */}
                 </ListItem>                      
               ))}
             </List>  
@@ -91,7 +108,7 @@ const App = ({choreList, getChoreList, isSignedIn, name, signIn}) => {
       <AppBar position="fixed" color="default" classes={{root: classes.footer}}>
         <Toolbar>
           {isSignedIn && (
-            <Button variant="contained" color="primary" disabled={true /* TODO: how many chores are selected? */}>Add chores</Button>
+            <Button variant="contained" color="primary" disabled={true /* TODO: how many chores are selected? */}>Save</Button>
           )}
         </Toolbar>
       </AppBar>
